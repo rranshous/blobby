6d00e98ea79314a977f30ee7d2c7314f688f80cc
Robby Ranshous <rranshous@gmail.com>
2011-12-26 18:35:42 -0500

    working

--- handlers/blobby.py
+++ handlers/blobby.py
@@ -1,4 +1,6 @@
 from tgen.blobby import Blobby, ttypes as o
+from hashlib import sha1
+import os.path
 
 class BlobbySimpleDiskHandler(object):
     """
@@ -10,11 +12,15 @@ class BlobbySimpleDiskHandler(object):
     ## CODE ASSUMES ONLY ONE SERVICE RUNNING AGAINST THE
     ## SAME DATA SOURCE
 
+    def __init__(self, save_root):
+        self.save_root = save_root
+
     def get_data(self, bhash):
         try:
             path = self.get_data_path(bhash)
             if not os.path.exists(path):
-                return None
+                # we could use a raise here instead
+                return ''
             else:
                 with open(path,'r') as fh:
                     return fh.read()
@@ -28,9 +34,11 @@ class BlobbySimpleDiskHandler(object):
             with open(path,'w') as fh:
                 fh.write(data)
         except Exception, ex:
-            raise o.Exception('oException set: %s %s' % (bhash,ex))
+            raise o.Exception('oException set: %s %s' % (len(data),ex))
 
-    def delete_set(self, bhash):
+        return bhash
+
+    def delete_data(self, bhash):
         path = self.get_data_path(bhash)
         try:
             if not os.path.exists(path):
@@ -38,7 +46,19 @@ class BlobbySimpleDiskHandler(object):
             os.unlink(path)
         except Exception, ex:
             raise o.Exception('oException delete: %s %s' % (bhash,ex))
+        return True
+
+
+    def get_data_path(self, bhash):
+        return os.path.join(self.save_root,bhash)
+
+    def get_data_bhash(self, data):
+        return sha1(data).hexdigest()
 
 def run():
     from lib.discovery import serve_service
-    serve_service(Blobby, BlobbySimpleDiskHandler())
+    save_root = os.path.abspath(os.path.join(
+                    os.path.dirname(os.path.abspath(__file__)),
+                    '../data'))
+    print 'save root: %s' % save_root
+    serve_service(Blobby, BlobbySimpleDiskHandler(save_root))
